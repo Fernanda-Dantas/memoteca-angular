4d8927b6b8fc6bff86bec9a1d672adbe83b4d9e3
Fernanda-Dantas <fer.dantas07@gmail.com>
2024-08-23 12:34:23 -0300

    Paginação e HttpParams

--- src/app/componentes/pensamentos/pensamento/pensamento.component.service.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Pensamento } from "./pensamento";
 import { Observable } from "rxjs";
@@ -9,13 +9,23 @@ import { Observable } from "rxjs";
 })
 export class PensamentoService {
 
-   private readonly API = 'http://localhost:3000/pensamentos'
+    private readonly API = 'http://localhost:3000/pensamentos'
 
     constructor(
-        private http: HttpClient) {}
+        private http: HttpClient) { }
 
-    listar(): Observable<Pensamento[]> {
-        return this.http.get<Pensamento[]>(this.API)
+    listar(pagina: number): Observable<Pensamento[]> {
+
+        const itensporPagina = 6;
+
+        let params = new HttpParams().set("_page", pagina).set("_limit", itensporPagina)
+
+        // pegou do json server
+        // return this.http.get<Pensamento[]>(
+        //    `${this.API}?_page=${pagina}&_limit=${itensporPagina}` 
+        // )
+
+        return this.http.get<Pensamento[]>(this.API, { params })
     }
 
     criar(pensamento: Pensamento): Observable<Pensamento> {
